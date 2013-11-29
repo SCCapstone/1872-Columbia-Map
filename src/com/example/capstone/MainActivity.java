7afc59a9b9a252e49576fe90be0b4d688724f62e
fagerbur <fagerbur@email.sc.edu>
2013-11-29 16:00:09 -0500

    updated bounds so map can't be dragged off screen

--- src/com/example/capstone/MainActivity.java
+++ src/com/example/capstone/MainActivity.java
@@ -10,91 +10,137 @@ import android.util.FloatMath;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
+	
 	ImageView imageDetail;
-	Matrix matrix = new Matrix();
-	Matrix savedMatrix = new Matrix();
-	PointF startPoint = new PointF();
-	PointF midPoint = new PointF();
-	float oldDist = 1f;
-	static final int NONE = 0;
-	static final int DRAG = 1;
-	static final int ZOOM = 2;
-	int mode = NONE;
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+
+    private static final float MIN_ZOOM = 1f;
+    private static final float MAX_ZOOM = 5f;
+
+    private Matrix matrix = new Matrix();
+    private Matrix savedMatrix = new Matrix();
+
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+
+    private int mode = NONE;
+    private float oldDistance = 1f;
+    private float dx; // postTranslate X distance
+	private float dy; // postTranslate Y distance
+	private float[] matrixValues = new float[9];
+	float matrixX = 0; // X coordinate of matrix inside the ImageView
+	float matrixY = 0; // Y coordinate of matrix inside the ImageView
+	float width = 0; // width of drawable
+	float height = 0; // height of drawable
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		setContentView(R.layout.activity_main);
 		
 		
 		imageDetail = (ImageView) findViewById(R.id.Map);
+		
 		/** * set on touch listener on image */
 		imageDetail.setOnTouchListener(new View.OnTouchListener() {
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				ImageView view = (ImageView) v;
-				System.out.println("matrix=" + savedMatrix.toString());
-				switch (event.getAction() & MotionEvent.ACTION_MASK) {
-				case MotionEvent.ACTION_DOWN:
-					savedMatrix.set(matrix);
-					startPoint.set(event.getX(), event.getY());
-					mode = DRAG;
-					break;
-				case MotionEvent.ACTION_POINTER_DOWN:
-					oldDist = spacing(event);
-					if (oldDist > 10f) {
-						savedMatrix.set(matrix);
-						midPoint(midPoint, event);
-						mode = ZOOM;
-					}
-					break;
-				case MotionEvent.ACTION_UP:
-				case MotionEvent.ACTION_POINTER_UP:
-					mode = NONE;
-					break;
-				case MotionEvent.ACTION_MOVE:
-					if (mode == DRAG) {
-						matrix.set(savedMatrix);
-						matrix.postTranslate(event.getX() - startPoint.x,
-								event.getY() - startPoint.y);
-					} else if (mode == ZOOM) {
-						float newDist = spacing(event);
-						if (newDist > 10f) {
-							matrix.set(savedMatrix);
-							float scale = newDist / oldDist;
-							matrix.postScale(scale, scale, midPoint.x,
-									midPoint.y);
-						}
-					}
-					break;
-				}
-				
-				
-				view.setImageMatrix(matrix);
-				return true;
-			}
+			  public boolean onTouch(View view, MotionEvent event) {
+        ImageView imageView = (ImageView)view;
 
-			@SuppressLint("FloatMath")
-			private float spacing(MotionEvent event) 
-			{
-				float x = event.getX(0) - event.getX(1);
-				float y = event.getY(0) - event.getY(1);
-				return FloatMath.sqrt(x * x + y * y);
-			}
+        switch(event.getAction() & MotionEvent.ACTION_MASK) {
 
-			private void midPoint(PointF point, MotionEvent event) 
-			{
-				float x = event.getX(0) + event.getX(1);
-				float y = event.getY(0) + event.getY(1);
-				point.set(x / 2, y / 2);
-			}
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDistance = spacing(event);
+                if(oldDistance > 10f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if(mode == DRAG) {
+                    matrix.set(savedMatrix);
+
+        matrix.getValues(matrixValues);
+        matrixX = matrixValues[2];
+        matrixY = matrixValues[5];
+        width = matrixValues[0] * (((ImageView) view).getDrawable()
+                                .getIntrinsicWidth());
+        height = matrixValues[4] * (((ImageView) view).getDrawable()
+                                .getIntrinsicHeight());
+
+        dx = event.getX() - start.x;
+        dy = event.getY() - start.y;
+
+        //if image will go outside left bound
+        if (matrixX + dx < 0){
+            dx = -matrixX;
+        }
+        //if image will go outside right bound
+        if(matrixX + dx + width > view.getWidth()){
+            dx = view.getWidth() - matrixX - width;
+        }
+        //if image will go oustside top bound
+        if (matrixY + dy < 0){
+            dy = -matrixY;
+        }
+        //if image will go outside bottom bound
+        if(matrixY + dy + height > view.getHeight()){
+            dy = view.getHeight() - matrixY - height;
+        }
+        matrix.postTranslate(dx, dy);   
+                }
+                else if(mode == ZOOM) {
+                    float newDistance = spacing(event);
+                    if(newDistance > 10f) {
+                        matrix.set(savedMatrix);
+                        float scale = newDistance / oldDistance;
+                        float[] values = new float[9];
+                        matrix.getValues(values);
+                        float currentScale = values[Matrix.MSCALE_X];
+                        if(scale * currentScale > MAX_ZOOM) 
+                            scale = MAX_ZOOM / currentScale;
+                        else if (scale * currentScale < MIN_ZOOM)
+                            scale = MIN_ZOOM / currentScale;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+        imageView.setImageMatrix(matrix);
+        return true;
+    }
+
+    @SuppressLint("FloatMath")
+	private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return FloatMath.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        point.set((event.getX(0) + event.getX(1)) / 2, (event.getY(0) + event.getY(1)) / 2);
+    }
+
 		});
 	}
 
@@ -106,5 +152,6 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
 
 }
