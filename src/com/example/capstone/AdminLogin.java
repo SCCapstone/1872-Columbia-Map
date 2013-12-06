9767f3a6f728f0b10ffd4ec3aa516dc37f2e2203
davidbare <David@192.168.1.104>
2013-12-06 07:51:53 -0500

    testing admin page functionality

--- src/com/example/capstone/AdminLogin.java
+++ src/com/example/capstone/AdminLogin.java
@@ -4,13 +4,30 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.os.Handler;
+import android.util.FloatMath;
+import android.view.MotionEvent;
+import android.widget.Gallery.LayoutParams;
+import android.widget.ImageView;
 
 public class AdminLogin extends Activity {
 
+	public final int popupWidth = 700;    
+	public final int popupHeight = 700;
+	public boolean popupon = false;	
 	
 	@Override	
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,7 @@ public class AdminLogin extends Activity {
 	EditText UserNameEdit = (EditText) findViewById(R.id.usernameedit);
 	EditText PasswordEdit = (EditText) findViewById(R.id.passwordedit);
 	Button login = (Button) findViewById(R.id.login);
+	Button createlogin = (Button) findViewById(R.id.createlogin);
 	Typeface TradeGothic18 = Typeface.createFromAsset(getAssets(),"TradeG18.ttf");
 	UserName.setTypeface(TradeGothic18);
 	Password.setTypeface(TradeGothic18);
@@ -31,8 +49,43 @@ public class AdminLogin extends Activity {
 	PasswordEdit.setTypeface(TradeGothic18);
 	login.setTypeface(TradeGothic18);
 	LoginTitle.setTypeface(TradeGothic18);
-	
-	
+											
+	createlogin.setOnClickListener(new OnClickListener() 
+	{
+		@Override     
+		public void onClick(View v) 
+		{			
+			popupon=true;
+
+			LayoutInflater layoutInflator = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+			View layout = layoutInflator.inflate(R.layout.create_login, null);
+
+			// Creating the PopupWindow 
+			final PopupWindow popup = new PopupWindow(layout, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);        			
+			popup.setContentView(layout);    
+			popup.setWidth(popupWidth);    
+			popup.setHeight(popupHeight);    
+			popup.setFocusable(true);        			
+			
+			popup.showAtLocation(layout, Gravity.CENTER, 0,0);
+    
+			Button close = (Button) layout.findViewById(R.id.close);   
+													
+			close.setOnClickListener(new OnClickListener() 
+			{
+				@Override     
+						
+				public void onClick(View v) 
+				{				  
+					popup.dismiss();	
+					popupon=false;
+				}								
+			});
+			popup.dismiss();	
+			popupon=false;
+		}								
+	});	
+		
 	}
 	
 	@Override
