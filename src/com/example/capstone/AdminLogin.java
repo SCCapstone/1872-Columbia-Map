22d16af9f420a70e5d2010cc6c454f2b4becc93b
oa2013 <agafonoo@email.sc.edu>
2014-02-13 21:26:54 -0500

    Removed the CreateAdminLogin class, created a Google+ login button in the AdminLogin class. Anticipating problems with Google Play services (can't test this with an emulator).

--- src/com/example/capstone/AdminLogin.java
+++ src/com/example/capstone/AdminLogin.java
@@ -1,5 +1,7 @@
 package com.example.capstone;
 
+import com.example.capstone.GoogleLoginActivity;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -22,45 +24,26 @@ public class AdminLogin extends Activity {
 	@Override	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
-	super.onCreate(savedInstanceState);	
-	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-	setContentView(R.layout.login_main);
-
-	TextView LoginTitle = (TextView) findViewById(R.id.logintitle);
-	TextView UserName = (TextView) findViewById(R.id.username);
-	TextView Password = (TextView) findViewById(R.id.password);
-	EditText UserNameEdit = (EditText) findViewById(R.id.usernameedit);
-	EditText PasswordEdit = (EditText) findViewById(R.id.passwordedit);
-	Button login = (Button) findViewById(R.id.login);
-	Button createlogin = (Button) findViewById(R.id.createlogin);
-	Typeface TradeGothic18 = Typeface.createFromAsset(getAssets(),"TradeG18.ttf");
-	UserName.setTypeface(TradeGothic18);
-	Password.setTypeface(TradeGothic18);
-	UserNameEdit.setTypeface(TradeGothic18);
-	PasswordEdit.setTypeface(TradeGothic18);
-	login.setTypeface(TradeGothic18);
-	LoginTitle.setTypeface(TradeGothic18);
-											
-	createlogin.setOnClickListener(new OnClickListener() 
-	{
-		@Override     
-		public void onClick(View view) 
-		{						
-			//start the CreateAdminLogin activity
-			Intent adminIntent = new Intent(AdminLogin.this, CreateAdminLogin.class);
-			startActivity(adminIntent);
-			
-			Log.d("AdminLogin", "After intent");
-		}								
-	});	
-		
-	}
+		super.onCreate(savedInstanceState);	
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		setContentView(R.layout.login_main);
 	
+		TextView LoginTitle = (TextView) findViewById(R.id.logintitle);
+		Typeface TradeGothic18 = Typeface.createFromAsset(getAssets(),"TradeG18.ttf");
+		LoginTitle.setTypeface(TradeGothic18);
+											
+	}
+	public void launchGoogleLogin(View v)
+	{
+	        AdminLogin.this.startActivity(new Intent(AdminLogin.this, GoogleLoginActivity.class));
+	        AdminLogin.this.finish();
+    }
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+			// Inflate the menu; this adds items to the action bar if it is present.
+			getMenuInflater().inflate(R.menu.main, menu);
+			return true;
 	}
 }
